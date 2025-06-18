b531a3c49b06f571ff0616bf9ca9d114f5b3009b
MatteoBollecchino <matthbolle@gmail.com>
2025-06-18 20:10:14 +0200

    Update

--- GrafoPesato.py
+++ GrafoPesato.py
@@ -1,30 +1,41 @@
 class GrafoPesato:
     def __init__(self):
-        self.nodi = {}
+        self.nodes = {}
 
-    def aggiungi_nodo(self, nodo):
-        if nodo not in self.nodi:
-            self.nodi[nodo] = {}
+    def add_node(self, node):
+        if node not in self.nodes:
+            self.nodes[node] = {}
 
-    def aggiungi_arco(self, da_nodo, a_nodo, peso):
-        self.aggiungi_nodo(da_nodo)
-        self.aggiungi_nodo(a_nodo)
-        self.nodi[da_nodo][a_nodo] = peso
+    def add_edge(self, from_node, to_node, cvss_score, epss_score):
+        if cvss_score < 0 or cvss_score > 10 :
+            print(repr("CVSS score errato"))
+            return
+        
+        if epss_score < 0 or epss_score > 1 :
+            print(repr("EPSS score errato"))
+            return
 
-    def ottieni_peso(self, da_nodo, a_nodo):
-        if da_nodo in self.nodi and a_nodo in self.nodi[da_nodo]:
-            return self.nodi[da_nodo][a_nodo]
+        self.add_node(from_node)
+        self.add_node(to_node)
+        self.nodes[from_node][to_node] = self.get_unmitigated_risk(cvss_score, epss_score)
+
+    def get_weight(self, from_node, to_node):
+        if from_node in self.nodes and to_node in self.nodes[from_node]:
+            return self.nodes[from_node][to_node]
         else:
             return None
 
-    def ottieni_vicini(self, nodo):
-        if nodo in self.nodi:
-            return self.nodi[nodo]
+    def get_neighbours(self, node):
+        if node in self.nodes:
+            return self.nodes[node]
         else:
             return {}
+        
+    def get_unmitigated_risk(self,cvss, epss):
+        return cvss*epss
 
     def __str__(self):
         s = ""
-        for nodo, vicini in self.nodi.items():
-            s += f"Nodo {nodo}: {vicini}\n"
+        for node, neighbours in self.nodes.items():
+            s += f"Nodo {node}: {neighbours}\n"
         return s
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import StackelbergSecurityGameEnv as ssg
 import GrafoPesato as gp
 import numpy as np
 
-
 if __name__ == "__main__":
     env = ssg.StackelbergSecurityGameEnv(n_targets=3)
 
@@ -19,11 +18,13 @@ if __name__ == "__main__":
     grafo = gp.GrafoPesato()
 
     # Aggiunta di nodi e archi
-    grafo.aggiungi_arco('A', 'B', 4)
-    grafo.aggiungi_arco('A', 'C', 2)
-    grafo.aggiungi_arco('B', 'C', 5)
-    grafo.aggiungi_arco('B', 'D', 10)
-    grafo.aggiungi_arco('C', 'D', 3)
+    grafo.add_edge('A', 'B', 1, 3)
+    grafo.add_edge('A', 'C', 2, 4)
+    grafo.add_edge('B', 'C', 20, 1)
+    grafo.add_edge('B', 'D', 10, 3)
+    grafo.add_edge('C', 'D', 3, 4)
     
     # Stampa del grafo
     print(grafo)
+
+    print(grafo.get_neighbours('A'))
