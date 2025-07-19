a1c726f91feac6d688dd18ecc5fbc3e669faba07
MatteoBollecchino <matthbolle@gmail.com>
2025-07-19 19:38:05 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,13 +3,14 @@ import networkx as nx
 import random
 
 class Utils:
-    def get_paths(grafo: nx.DiGraph, origine):
+    # Restituisce tutti i path che hanno origine in 'source'
+    def get_paths(grafo: nx.DiGraph, source):
 
         paths = list()
 
         for node in grafo.nodes:
             target = node
-            paths = paths + list(nx.all_simple_paths(grafo, source=origine, target=target))
+            paths = paths + list(nx.all_simple_paths(grafo, source=source, target=target))
 
         return paths
     
@@ -21,6 +22,7 @@ class Utils:
         # oppure (impact/10 + afr)/2
         return round(afr * 0.6 + impact_norm * 0.4, 4)
 
+    # Restituisce il rischio del path passato per parametro
     def get_path_risk(graph: nx.DiGraph, path: list):
         risk_values = list()
 
@@ -31,6 +33,16 @@ class Utils:
             risk_values = risk_values + Utils.get_node_risk(graph.nodes[node], afr)
 
         return max(risk_values)
+    
+    # da testare
+    # Restituisce il path che tra tutti ha il rischio maggiore associato
+    def get_maximum_risk_path(graph: nx.DiGraph, node_source: str):
+        paths = Utils.get_paths(graph, node_source)
+        risk_paths = list()
 
-    def get_maximum_risk_path():
-        pass
+        for path in paths:
+            risk_paths.append(Utils.get_path_risk(path))
+
+        index_max_risk_path = risk_paths.index(max(risk_paths))
+
+        return paths[index_max_risk_path]
